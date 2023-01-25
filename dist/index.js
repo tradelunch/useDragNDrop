544f840a3cc40ab825aab45c6b90d908d1dab807
Tio Lim <tio.lim@adriel.ai>
2023-01-25 14:53:08 +0900

    fix: build dist

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.useDragNDrop = exports.Draggable = void 0;
-var Draggable_1 = require("./Draggable");
-Object.defineProperty(exports, "Draggable", { enumerable: true, get: function () { return Draggable_1.Draggable; } });
+exports.useDragNDrop = void 0;
 var useDragNDrop_1 = require("./useDragNDrop");
 Object.defineProperty(exports, "useDragNDrop", { enumerable: true, get: function () { return useDragNDrop_1.useDragNDrop; } });
 //# sourceMappingURL=index.js.map
